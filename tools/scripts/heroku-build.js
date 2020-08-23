afcd30ad6341df2122031f6038b4c9891effa97c
Loïc <lo.pennequin@gmail.com>
2020-08-23 22:17:52 +0200

    change build scripts

---
+++ tools/scripts/heroku-build.js
@@ -0,0 +1,26 @@
+const { exec } = require('child_process');
+
+console.log('Starting build Script...');
+console.log(`APP_NAME is ${process.env.APP_NAME}`);
+
+function execWrap(command) {
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
+  });
+}
+
+switch (process.env.APP_NAME) {
+  case 'client':
+    execWrap('npm run build:client');
+    break;
+  case 'api':
+    execWrap('npm run build:api');
+    break;
+  default:
+    console.log('no build process for this app');
+}
