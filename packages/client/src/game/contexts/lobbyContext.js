62e18288b7c4f575308deec0fe73e45c582dbdaf
Loïc <lo.pennequin@gmail.com>
2020-08-24 13:51:45 +0200

    fix: remove loading on lobby when game has finished

--- packages/client/src/game/contexts/lobbyContext.js
+++ packages/client/src/game/contexts/lobbyContext.js
@@ -1,9 +1,19 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
+import { useWebsockets } from '@core/hooks/useWebsockets';
+import { constants } from '@c4/shared';
+
+const { EVENTS } = constants;
 
 export const lobbyContext = createContext(null);
 
 export function LobbyProvider({ children }) {
   const [isGameLoading, setIsGameLoading] = useState(false);
+
+  const { on } = useWebsockets();
+
+  useEffect(() => {
+    on(EVENTS.GAME_HAS_FINISHED, () => setIsGameLoading(false));
+  }, [on]);
 
   return (
     <lobbyContext.Provider
