caf2c9d8572c6e792cf9e15e36e7382334d25e46
Loïc <lo.pennequin@gmail.com>
2020-09-04 22:14:06 +0200

    typo

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -9,6 +9,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-// if (process.env.NODE_ENV === 'production') {
-//   serviceWorker.register();
-// }
+if (process.env.NODE_ENV === 'production') {
+  serviceWorker.unregister();
+}
